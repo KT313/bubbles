181071ae43157611fced3db476656323e521118d
KT313 <73772644+KT313@users.noreply.github.com>
2023-11-08 09:01:04 +0100

    Update bubbles.py

--- bubbles.py
+++ bubbles.py
@@ -21,8 +21,9 @@ class BaseModel(nn.Module):
         # print(f"shape of BaseModel input: {x.shape}")
         # Reshape the input while keeping the batch size the same
         batch_size = x.size(0)
-        x = x.view(batch_size, -1).to("cuda:0")
-      # print(f"reshaped BaseModel input: {x.shape}, layer shape: {self.layer_input.weight.shape}")
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        x = x.view(batch_size, -1).to(device)
+        # print(f"reshaped BaseModel input: {x.shape}, layer shape: {self.layer_input.weight.shape}")
         x = self.relu(self.layer_input(x))
         for layer_middle in self.layers_middle:
             x = self.relu(layer_middle(x))
@@ -49,7 +50,8 @@ class InputBaseModel(nn.Module):
         # print(f"shape of BaseModel input: {x.shape}")
         # Reshape the input while keeping the batch size the same
         batch_size = x.size(0)
-        x = x.view(batch_size, -1).to("cuda:0")
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        x = x.view(batch_size, -1).to(device)
         # print(f"reshaped BaseModel input: {x.shape}")
         x = self.relu(self.layer_input(x))
         for layer_middle in self.layers_middle:
@@ -78,7 +80,8 @@ class OutputBaseModel(nn.Module):
         # print(f"shape of BaseModel input: {x.shape}")
         # Reshape the input while keeping the batch size the same
         batch_size = x.size(0)
-        x = x.view(batch_size, -1).to("cuda:0")
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        x = x.view(batch_size, -1).to(device)
         # print(f"reshaped BaseModel input: {x.shape}, layer shape: {self.layer_input.weight.shape}")
         x = self.relu(self.layer_input(x))
         for layer_middle in self.layers_middle:
@@ -98,13 +101,13 @@ class OutputBaseModel(nn.Module):
 
 
 class Bubble(nn.Module):
-    def __init__(self, num_connections, connection_size, model_size, model_layers, id,architecture=None, input_size=None, output_size=None, is_base_bubble=False):
+    def __init__(self, model, num_connections, connection_size,id, is_base_bubble=False):
         super(Bubble, self).__init__()
         # print(f"bubble: {connection_size}")
         # Initialize the neural network based on the specified architecture
         self.id = id
         self.num_connections = num_connections
-        self.neural_network = BaseModel(connections = self.num_connections, connection_size = connection_size, model_size = model_size, model_layers = model_layers)
+        self.neural_network = model
         self.is_base_bubble = is_base_bubble
         self.inputs = [[0 for _ in range(connection_size)] for _ in range(num_connections)]
         self.outputs = [[0 for _ in range(connection_size)] for _ in range(num_connections)] # stores nn result so it can be passed on next tick
@@ -128,26 +131,36 @@ class Bubble(nn.Module):
         pass
 
 class System(nn.Module):
-    def __init__(self, bubble_count = 5, model_size = 1024, model_layers = 3,output_size = 4096, ticks_to_run = 3, connection_size = 32, base_bubble = None, bubbles = [], outside_input = np.zeros(64).astype(float)):
+    def __init__(self, bubble_count = None, ticks_to_run = 3, connection_size = 32, base_bubble = None, bubbles = [], outside_input = np.zeros(64).astype(float), output_size = 32, input_dtype = torch.float):
         super(System, self).__init__()
         # print(f"system: {connection_size}")
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.num_bubbles = 1 + len(bubbles) # 1 for base_bubble
+        self.num_connections = self.num_bubbles
         if base_bubble == None:
-            self.base_bubble = Bubble(is_base_bubble=True, num_connections = bubble_count, connection_size = connection_size, model_size = model_size, model_layers = model_layers, id = 0)
+            print("error, base bubble missing")
+            # self.base_bubble = Bubble(is_base_bubble=True, num_connections = bubble_count, connection_size = connection_size, model_size = model_size, model_layers = model_layers, id = 0)
         else:
-            self.base_bubble = base_bubble
+            self.base_bubble = Bubble(model = base_bubble, id = 0, connection_size = connection_size, num_connections = self.num_connections, is_base_bubble = True)
+
         if bubbles == []:
-            for i in range(bubble_count-1):
-                bubbles.append(Bubble(num_connections = bubble_count, connection_size = connection_size, model_size = model_size, model_layers = model_layers, id = i+1))
-            self.bubbles = nn.ModuleList(bubbles)
+            # for i in range(bubble_count-1):
+            #     bubbles.append(Bubble(num_connections = bubble_count, connection_size = connection_size, model_size = model_size, model_layers = model_layers, id = i+1))
+            # self.bubbles = nn.ModuleList(bubbles)
+            print("error, bubbles missing")
         else:
-            self.bubbles = nn.ModuleList(bubbles)
+            bubble_merker = []
+            counter = 1
+            for part in bubbles:
+                bubble_merker.append(Bubble(model = part, id = counter, connection_size = connection_size, num_connections = self.num_connections))
+                counter += 1
+            self.bubbles = nn.ModuleList(bubble_merker)
         self.ticks = 0
         self.connection_size = connection_size
         self.ticks_to_run = ticks_to_run
         self.outside_input = outside_input
         self.output_size = output_size
-        self.outputmodel = OutputBaseModel(connections = 1, connection_size = connection_size, model_size = model_size, model_layers = model_layers, output_size = self.output_size).to("cuda:0")
-        self.num_bubbles = 1 + len(self.bubbles) # 1 for base_bubble
+        self.outputmodel = OutputBaseModel(connections = 1, connection_size = connection_size, model_size = 128, model_layers = 2, output_size = self.output_size).to(self.device)
         self.connection_buffer = torch.zeros((batch_size, self.num_bubbles, self.num_bubbles, self.connection_size), requires_grad=False) # saves outputs of bubbles and provides them as inputs next tick. [0, 1] -> output from bubble 0, to be used for bubble 1
 
     def tick(self, outside_input):
@@ -159,7 +172,10 @@ class System(nn.Module):
         if self.ticks == 0:
             # Only the base bubble processes input
             # print(f"outside input shape \nbefore unsqueezing: {outside_input.shape}\nafter unsqueezing: {outside_input.unsqueeze(1).shape}\ntensor to cat: {torch.zeros((outside_input.shape[0] ,self.num_bubbles - 1, outside_input.size(0))).shape}")
-            input_tensor = torch.cat([outside_input.unsqueeze(1), torch.zeros((outside_input.shape[0], self.num_bubbles - 1, outside_input.shape[1])).to("cuda:0")], dim=1)
+            filler_tensor = torch.zeros((outside_input.shape[0], self.num_bubbles - 1, self.connection_size))
+            # print(f"filler tensor shape: {filler_tensor.shape}\ninput tensor shape: {outside_input.shape}\ninput tensor unseqeezed shape: {outside_input.unsqueeze(1).shape}")
+            input_tensor = torch.cat([outside_input.unsqueeze(1), filler_tensor.to(self.device)], dim=1)
+            # print(f"shape after filling: {input_tensor.shape}")
             # # print(f"self.outside_input {self.outside_input.shape}, resulting input {input.shape}")
             output = self.base_bubble.do_tick(inputs = input_tensor)
             # print(f"output shape from bubble: {output.shape}\nshape connection buffer: {self.connection_buffer.shape}")
@@ -187,16 +203,19 @@ class System(nn.Module):
         start_run_time = time.time()
         tick_time = []
         wrong_batch_size = False
-      # print(x.shape)
+        # print(x.shape)
+        # if the outside input is smaller than total connection size, fill missing values with 0
         if x.shape[0] < batch_size:
             wrong_batch_size = True
             merker_batch_size = x.shape[0]
             c = torch.zeros(batch_size, x.shape[1])
             c[:x.shape[0]] = x
             x = c
-        a = torch.zeros(batch_size, self.connection_size).to("cuda:0")
-        a[:, :32] = x
+        a = torch.zeros(batch_size, self.connection_size).to(self.device)
+        # print(a.shape)
+        a[:, :x.shape[-1]] = x
         x = a
+        # print(x.shape)
       # print(x.shape)
         # Run the system for a certain number of ticks
         for _ in range(self.ticks_to_run):
